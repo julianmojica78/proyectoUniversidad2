92aa3156ea01c8d6bc3d6cb64793474b1b1a8c15
Jonathan8088 <sosa8088@hotmail.com>
2019-10-20 21:57:42 -0500

    validaciones de campos

--- proyectoUniversidades2-ejb/target/generated-sources/annotations/Entity/Universidad_.java
+++ proyectoUniversidades2-ejb/target/generated-sources/annotations/Entity/Universidad_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-19T15:46:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-19T20:56:43")
 @StaticMetamodel(Universidad.class)
 public class Universidad_ { 
 
